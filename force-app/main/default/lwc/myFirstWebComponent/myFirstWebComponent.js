b2c9a2618e1ddddec8e0e4ba862d89ca72f0d054
Keith Poulos <keith.poulos@ymcawichita.org>
2024-06-28 20:41:49 +0000

    pre-account creator

--- force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
+++ force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
@@ -1,12 +1,27 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue'; 
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+import { getRecord, getFieldValue} from 'lightning/uiRecordApi';
 
+export class WireGetRecordProperty extends LightningElement{
+    @api recordId;
+    @wire(getRecord, { recordId: '$recordId', fields: [NAME_FIELD]})
+    account;
+    get name(){
+        return getFieldValue(this.account.data, NAME_FIELD);
+    }    
+}
 export default class MyFirstWebComponent extends LightningElement {
 
+    @api recordId;
+    @wire(getRecord, { recordId: '$recordId', fields: [NAME_FIELD]})
+    account;
+    get name(){
+        return getFieldValue(this.account.data, NAME_FIELD);
+    }
     objectApiName = ACCOUNT_OBJECT;
     fields = [NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD];
     handleSuccess(event){
