07405cd8b9d9807b00933c4b859534c2c93b0036
Keith Poulos <keith.poulos@ymcawichita.org>
2024-07-03 14:03:47 +0000

    After Trailhead

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,4 +1,5 @@
 import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
 import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
@@ -14,4 +15,7 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+    get errors() {
+        return (this.contacts.error) ? reduceErrors(this.contacts.error) : [];
+    }
 }
