46d70c108b2b7d3b9185e59305ad574755bafa52
Keith Poulos <keith.poulos@ymcawichita.org>
2024-07-17 15:52:25 +0000

    Pre Removing Error

--- force-app/main/default/lwc/disconnectionNotice/disconnectionNotice.js
+++ force-app/main/default/lwc/disconnectionNotice/disconnectionNotice.js
@@ -1,27 +1,51 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { subscribe, unsubscribe, onError } from "lightning/empApi";
 
 
 export default class DisconnectionNotice extends LightningElement {
     subscription = {};
     status;
     identifier;
-    channelName;
+    channelName = "/event/Asset_Disconnection__e";
 
     connectedCallback() {
         this.handleSubscribe();
     }
 
     renderedCallback(){
-        
     }
 
-    handleSubscribe() {
+    handleNotificationEvent(event) {
+        const status = event.data.payload.Disconnected__c;
+        const identifier = event.data.payload.Asset_Identifier__c;
+        this.status = status;
+        this.identifier = identifier; 
+        
+        if(status === true){
+        this.showSuccessToast(identifier);
+        } else {
+            this.showErrorToast();
+        }
+    }
+
+    async handleSubscribe() {
         //Implement your subscribing solution here 
+        onError((error) => {
+            this.showErrorToast();
+            console.log("EMP API error reported by server: ", JSON.stringify(error));
+        });
+
+        this.subscription = await subscribe(  
+            this.channelName,
+            -1,
+            (event) => this.handleNotificationEvent(event)
+        );
     }
 
     disconnectedCallback() {
         //Implement your unsubscribing solution here
+        unsubscribe(this.subscription);
     }
 
     showSuccessToast(assetId) {
