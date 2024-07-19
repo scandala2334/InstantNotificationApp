e6078846dbd41eba63e5dcd04b10094b8658e950
Keith Poulos <keith.poulos@ymcawichita.org>
2024-07-19 15:22:41 +0000

    finally works

--- force-app/main/default/lwc/disconnectionNotice/disconnectionNotice.js
+++ force-app/main/default/lwc/disconnectionNotice/disconnectionNotice.js
@@ -1,28 +1,20 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { subscribe, unsubscribe, onError } from "lightning/empApi";
+import { subscribe, unsubscribe, onError } from 'lightning/empApi';
+
 
 export default class DisconnectionNotice extends LightningElement {
-
     subscription = {};
     status;
     identifier;
-    channelName = "/event/Asset_Disconnection__e";
+    channelName = '/event/Asset_Disconnection__e';
 
     connectedCallback() {
         this.handleSubscribe();
     }
 
     renderedCallback(){
-    }
-
-    handleSubscribe() {
-        //Implement your subscribing solution here 
-        this.subscription = subscribe (
-            this.channelName, 
-            -1, 
-            (message) => this.handleMessage(message)
-        )
+        
     }
 
     handleMessage (message){
@@ -32,7 +24,7 @@ export default class DisconnectionNotice extends LightningElement {
         this.identifier = identifier; 
         console.log('New message received: ', JSON.stringify(message));
 
-        const messageCallback = () => {
+        const messageCallback = function() {
             if (this.status === true){
                 console.log('In showToast True: ', JSON.stringify(this.identifier));
                 this.showSuccessToast(this.identifier);
@@ -44,13 +36,39 @@ export default class DisconnectionNotice extends LightningElement {
         messageCallback();
     }
 
+    handleSubscribe() {
+        //Implement your subscribing solution here 
+        const messageCallback = function (response) {
+            const status = response.data.payload.Disconnected__c;
+            const identifier = response.data.payload.Asset_Identifier__c;
+            this.status = status;
+            this.identifier = identifier; 
+            console.log('New message received: ', JSON.stringify(response));
+            if (this.status === true){
+                console.log('In showToast True: ', JSON.stringify(this.identifier));
+                this.showSuccessToast(this.identifier);
+            } else {
+                console.log('In showToast False: ', JSON.stringify(this.status));
+                this.showErrorToast();
+            }
+        };
+        subscribe(this.channelName, -1, messageCallback).then((response) => {
+            // Response contains the subscription information on subscribe call
+            console.log(
+                'Subscription request sent to: ',
+                JSON.stringify(response.channel)
+            );
+            this.subscription = response;
+            this.toggleSubscribeButton(true);
+        });
+    }
+
     disconnectedCallback() {
         //Implement your unsubscribing solution here
         unsubscribe(this.subscription);
     }
 
     showSuccessToast(assetId) {
-        console.log('In Toast True: ', JSON.stringify(this.status));
         const event = new ShowToastEvent({
             title: 'Success',
             message: 'Asset Id '+assetId+' is now disconnected',
@@ -61,7 +79,6 @@ export default class DisconnectionNotice extends LightningElement {
     }
 
     showErrorToast() {
-        console.log('In Toast False: ', JSON.stringify(this.status));
         const event = new ShowToastEvent({
             title: 'Error',
             message: 'Asset was not disconnected. Try Again.',
